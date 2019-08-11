21ed810d5638e94d43cc0e8717845659f81ef88b
liangtl2 <liangtl2@lenovo.com>
2019-08-11 13:53:28 +0800

    first commit

---
+++ src/main/java/com/example/myWeb/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.example.myWeb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+ @Override
+ protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+                                 //Application的类名
+     return application.sources(MyWebApplication.class);
+ }
+
+}
---
+++ src/main/resources/public/precache-manifest.abeb1c7935d68ca6cf92307e9903df87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6e59621cb7fdb53c0ca8f55defe9bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80b14c93db1d1f2e833d",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cc1612339c7208afe06e",
+    "url": "./static/js/2.c0065bbc.chunk.js"
+  },
+  {
+    "revision": "80b14c93db1d1f2e833d",
+    "url": "./static/js/main.c76aae28.chunk.js"
+  },
+  {
+    "revision": "b1366c1ace47f604d702",
+    "url": "./static/js/runtime~main.88bfe61a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
